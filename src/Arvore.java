c688c279ad422b7d5dde85ae3d59dff0f0df9bd5
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-05-20 15:59:52 -0300

    ajustes base

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Arvore {
+public class Arvore {
+    private No raiz;
+
+
+    //inserir um elemento na árvore. útiliza um método auxiliar recursivo
+    public void inserir(int valor) {
+
+        if(raiz == null) {
+            raiz = new No(valor);
+        }else {
+            raiz.inserirNo(valor);
+        }
+    }
+
+    //executa o percurso em ordem
+    public void emOrdem() {
+        emOrdem_aux(raiz); //faço isso pq a raiz é privada
+    }
+    private void emOrdem_aux(No raiz) {
+        if(raiz == null) {
+            return;
+        }
+
+        emOrdem_aux(raiz.esq);
+        System.out.print(raiz.info+"\t");
+        emOrdem_aux(raiz.dir);
+
+    }
+
+    public void preOrdem() {
+        preOrdem_aux(raiz); //faço isso pq a raiz é privada
+    }
+    private void preOrdem_aux(No raiz) {
+        if(raiz == null) {
+            return;
+        }
+        System.out.print(raiz.info+"\t");
+        preOrdem_aux(raiz.esq);
+        preOrdem_aux(raiz.dir);
+
+    }
+
+    public void posOrdem() {
+        posOrdem_aux(raiz); //faço isso pq a raiz é privada
+    }
+    private void posOrdem_aux(No raiz) {
+        if(raiz == null) {
+            return;
+        }
+        posOrdem_aux(raiz.esq);
+        posOrdem_aux(raiz.dir);
+        System.out.print(raiz.info+"\t");
+
+
+    }
 }
--- src/No.java
+++ src/No.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class No {
+public class No {
+
+    //atributos
+    int info;
+    No esq;
+    No dir;
+
+    //construtor
+    public No(int info) {
+        this.info = info;
+        esq = null;
+        dir = null;
+    }
+
+    public void inserirNo(int valor) {
+        if(valor > info) {
+            if(dir == null) {
+                dir = new No(valor);
+            }else {
+                dir.inserirNo(valor);
+            }
+        }else {
+            if(esq == null) {
+                esq = new No(valor);
+            }else {
+                esq.inserirNo(valor);
+            }
+        }
+    }
+
+
 }
